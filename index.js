26562645ca70e51c686c28ad8321df45a26bf6e2
445212619@qqcom <beanjs>
2021-01-04 10:20:14 +0800

    1.Optimize ajv verification

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const AJV = require('ajv').default
 const kBeanifyAjv = Symbol.for('beanify.ajv')
+const kAjvBody = Symbol.for('ajv.body')
+const kAjvAttribute = Symbol.for('ajv.attribute')
+const kAjvResponse = Symbol.for('ajv.response')
 
 function buildAjvErrorsMsg (name, errs) {
   return errs
@@ -9,32 +12,41 @@ function buildAjvErrorsMsg (name, errs) {
     .join('\n')
 }
 
-function verification (name, schema, val) {
-  const ajv = this.$beanify[kBeanifyAjv]
-  const verification = ajv.compile(schema)
-  if (!verification(val)) {
-    throw new Error(buildAjvErrorsMsg(name, verification.errors))
+function verification (name, verifyCall, val) {
+  if (!verifyCall(val)) {
+    throw new Error(buildAjvErrorsMsg(name, verifyCall.errors))
   }
 }
 
 module.exports = async function (beanify, opts) {
   beanify[kBeanifyAjv] = new AJV(opts.ajv)
-  beanify.addHook('onBeforeHandler', async function (req, rep) {
-    const schema = this.schema || {}
 
+  beanify.addHook('onRoute', function (route) {
+    const ajv = route.$beanify[kBeanifyAjv]
+    const schema = route.schema || {}
     if (schema.body) {
-      verification.call(this, 'body', schema.body, req.body)
+      route[kAjvBody] = ajv.compile(schema.body)
+    }
+    if (schema.attribute) {
+      route[kAjvAttribute] = ajv.compile(schema.attribute)
+    }
+    if (schema.response) {
+      route[kAjvResponse] = ajv.compile(schema.response)
+    }
+  })
+
+  beanify.addHook('onBeforeHandler', async function (req, rep) {
+    if (this[kAjvBody]) {
+      verification('body', this[kAjvBody], req.body)
     }
 
-    if (schema.attribute) {
-      verification.call(this, 'attribute', schema.attribute, this.$attribute)
+    if (this[kAjvAttribute]) {
+      verification('attribute', this[kAjvAttribute], this.$attribute)
     }
   })
   beanify.addHook('onAfterHandler', function (req, rep) {
-    const schema = this.schema || {}
-
-    if (schema.response && !rep.$sent) {
-      verification.call(this, 'response', schema.response, rep.$data)
+    if (this[kAjvResponse]) {
+      verification('response', this[kAjvResponse], rep.$data)
     }
   })
 }
